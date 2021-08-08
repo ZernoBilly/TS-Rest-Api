3a407d193de73fb3f233c43a9492d07ddad2f2ae
ZernoBilly <heikki.kurppa@gmail.com>
2021-08-08 16:16:00 +0300

    first commit

---
+++ source/config/config.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const SERVER_HOSTNAME = process.env.SERVER_PORT || 'localhost ';
+const SERVER_PORT = process.env.SERVER_PORT || 5001;
+
+const SERVER = {
+    hostname: SERVER_HOSTNAME,
+    port: SERVER_PORT
+};
+const config = {
+    server: SERVER
+};
+export default config;
